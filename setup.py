e78aa2f23b9b25e7f60a19659e5b0f829a53b9c5
kevin santana <kejosant@espol.edu.ec>
2024-11-20 02:01:43 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+
+from setuptools import setup, find_packages
+
+def readme() -> str:
+    """Utility function to read the README.md.
+
+    Used for the `long_description`. It's nice, because now
+    1) we have a top level README file and
+    2) it's easier to type in the README file than to put a raw string in below.
+
+    Args:
+        nothing
+
+    Returns:
+        String of readed README.md file.
+    """
+    return open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+setup(
+    name='lightweight_clustered_energy_forecasts',
+    version='0.1.0',
+    author='kevin santana',
+    author_email='kejosant@espol.edu.ec',
+    description='A short description of the project.',
+    python_requires='>=3',
+    url='',
+    packages=find_packages(),
+    long_description=readme(),
+)
